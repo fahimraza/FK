56a76818c688089428c960c6e1733b15faea6e1b
fahimraza <fahimraza101@gmail.com>
2016-01-04 19:45:33 +0500

    Create pop-ad.js

---
+++ pop-ad.js
@@ -0,0 +1,14 @@
+$("#rev-ad,#shade").show().slideDown();
+  setTimeout(function(){
+      $("#rev-ad,#shade").show();        
+  },5000);
+$(document).ready(function(){
+    $("#hide2").click(function(){
+        $("#rev-ad,#shade").hide();
+    });
+});
+$(document).ready(function(){
+    $("#hide3").click(function(){
+        $("#notice2").hide();
+    });
+});
