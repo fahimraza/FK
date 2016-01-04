0cfcf79535654709b835d1ad69b0783a97a7fcd9
fahimraza <fahimraza101@gmail.com>
2016-01-04 19:53:48 +0500

    Update slidead.js

--- slidead.js
+++ slidead.js
@@ -2,17 +2,8 @@ setTimeout(function() {
     
     document.getElementById("popup").className="active";
     
-}, 5000);
+}, 90000);
 
 document.getElementById("close").onclick=function () {
     document.getElementById("popup").className="";
-}
-setTimeout(function() {
-    
-    document.getElementById("poprite").className="active";
-    
-}, 5000);
-
-document.getElementById("close").onclick=function () {
-    document.getElementById("poprite").className="";
 }
