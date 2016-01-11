a8e65c13956b100a8878a598bdeed28e3187910f
fahimraza <fahimraza101@gmail.com>
2016-01-11 11:37:10 +0500

    Update slidead.js

--- slidead.js
+++ slidead.js
@@ -2,7 +2,7 @@ setTimeout(function() {
     
     document.getElementById("popup").className="active";
     
-}, 100000);
+}, 90000);
 
 document.getElementById("close").onclick=function () {
     document.getElementById("popup").className="";
