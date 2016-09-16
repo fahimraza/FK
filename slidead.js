20333b855f435793ee43f647e3a34ced6ec61ba1
fahimraza <fahimraza101@gmail.com>
2016-09-16 15:01:12 +0500

    Delete slidead.js

--- slidead.js
+++
@@ -1,9 +0,0 @@
-setTimeout(function() {
-    
-    document.getElementById("popup").className="active";
-    
-}, 90000);
-
-document.getElementById("close").onclick=function () {
-    document.getElementById("popup").className="";
-}
