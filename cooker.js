ee5a2742e74252df5400b67229dd3f57d2b3df6e
fahimraza <fahimraza101@gmail.com>
2016-11-28 19:23:28 +0500

    Update cooker.js

--- cooker.js
+++ cooker.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 $('#x-fund').delay(42000).fadeIn(100);
   // If the 'hide cookie is not set we show the message
   if (!readCookie('hide')) {
-$('#fund').delay(20000).fadeIn(100);
+$('#fund').delay(10000).fadeIn(100);
   }
 
   // Add the event that closes the popup and sets the cookie that tells us to
