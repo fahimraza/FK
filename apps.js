712c7344c274bb2aa0c60135ae4d308807ca646c
fahimraza <fahimraza101@gmail.com>
2017-03-10 14:30:50 +0500

    Create apps.js

---
+++ apps.js
@@ -0,0 +1,22 @@
+        var Appnext = {
+          android_id: 'ADD_HERE_YOUR_PLACEMENT_ID',
+          ios_id: 'ADD_HERE_YOUR_PLACEMENT_ID',
+          parent_url:window.location.origin,
+          cnt: '',
+          pbk: '',
+          creative:'',
+          subid:'',
+          show_skip:'',
+          b_color:'',
+          auto_play:'',
+          b_title:'',
+          title:'' ,
+          ad_server:''    
+        };
+        (function(){
+           var _s=document.createElement('script');
+           _s.type='text/javascript';
+           _s.async=true;
+           _s.src='https://appnext.hs.llnwd.net/tools/tags/widget300x250/m.js';
+           (document.body)?document.body.appendChild(_s):document.head.appendChild(_s);         
+         })();
