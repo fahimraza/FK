4c250feb825ae3763ab6dff1b6c243fbe9c13bd5
fahimraza <fahimraza101@gmail.com>
2017-10-15 15:26:42 +0500

    Update 101helper.js

--- 101helper.js
+++ 101helper.js
@@ -1,31 +1,31 @@
 // Button trigger:
 $(function() {
     setTimeout(function() {
-        $('.addthis_button_compact').trigger('click');
+        $(&#39;.addthis_button_compact&#39;).trigger(&#39;click&#39;);
     }, 120000);
 });
 // Close related posts:
 $(document).ready(function(){
-  $(".x-feat").click(function() { 
-    $("#feat").fadeOut("slow");
+  $(&quot;.x-feat&quot;).click(function() { 
+    $(&quot;#feat&quot;).fadeOut(&quot;slow&quot;);
   });
 });
 // sticky nav and reloader:
 $(window).scroll(function(){ 
 var a = 2000;
 var pos = $(window).scrollTop();
-if(pos &gt; a) {$("#reloader").css({position: 'fixed',top:'5px',width: '23%'});}
-else {$("#reloader").css({position: 'relative',top:'10px',width: '300px'});}});
+if(pos &gt; a) {$(&quot;#reloader&quot;).css({position: &#39;fixed&#39;,top:&#39;5px&#39;,width: &#39;23%&#39;});}
+else {$(&quot;#reloader&quot;).css({position: &#39;relative&#39;,top:&#39;10px&#39;,width: &#39;300px&#39;});}});
 $(window).scroll(function(){ 
 var a = 1000;
 var pos = $(window).scrollTop();
-if(pos &gt; a) {$('.nav').css({position: 'fixed',top:'-5px',width: '14%'});}
+if(pos &gt; a) {$(&#39;.nav&#39;).css({position: &#39;fixed&#39;,top:&#39;-5px&#39;,width: &#39;14%&#39;});}
 else 
-{$('.nav').css({position: 'relative',width: 'initial'});}
+{$(&#39;.nav&#39;).css({position: &#39;relative&#39;,width: &#39;initial&#39;});}
 });
 // Related posts popup:
 $(window).load(function() {
   setTimeout(function() {
-      $('.related').addClass('show');
+      $(&#39;.related&#39;).addClass(&#39;show&#39;);
     }, 60000)
 });
