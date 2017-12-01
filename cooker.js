feb0c3548ef78490a4f8231bdcc6e1e54f1c90ec
fahimraza <fahimraza101@gmail.com>
2017-12-02 00:51:27 +0500

    Update cooker.js

--- cooker.js
+++ cooker.js
@@ -21,7 +21,7 @@ $('#fund').delay(20000).fadeIn(100);
 function createCookie(name,value,days) {
   if (days) {
     var date = new Date();
-    date.setTime(date.getTime()+(days*24*60*60*1000));
+    date.setTime(date.getTime()+(days*12*60*60*1000));
     var expires = "; expires="+date.toGMTString();
   }
   else var expires = "";
