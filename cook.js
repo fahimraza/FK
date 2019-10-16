359749dc658e7ec68915eedcb9b516c2c5b86512
fahimraza <fahimraza101@gmail.com>
2019-10-16 03:10:35 -0700

    Update cook.js

--- cook.js
+++ cook.js
@@ -1,45 +1,6 @@
 $(document).ready(function() {
-  // If the 'hide cookie is not set we show the message
-  if (readCookie('pa_10826_1_last_url')) {
-$('#ad-watch-pop').delay(10000).fadeIn(100);
-}
-  // Add the event that closes the popup and sets the cookie that tells us to
-  // not show it again until one day has passed.
-  $(document).ready(function() {
-    if (!readCookie('pa_10826_1_last_url')) {
-    setTimeout(function() {
-    createCookie('pa_10826_1_last_url', true, 1)
-    return false;
-      }, 10000);
-    };
-  });
-
-});
-
-// ---
-// And some generic cookie logic
-// ---
-function createCookie(name,value,days) {
-  if (days) {
-    var date = new Date();
-    date.setTime(date.getTime()+(days*20*60*1000));
-    var expires = "; expires="+date.toGMTString();
-  }
-  else var expires = "";
-  document.cookie = name+"="+value+expires+"; path=/";
-}
-
-function readCookie(name) {
-  var nameEQ = name + "=";
-  var ca = document.cookie.split(';');
-  for(var i=0;i < ca.length;i++) {
-    var c = ca[i];
-    while (c.charAt(0)==' ') c = c.substring(1,c.length);
-    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-  }
-  return null;
-}
-
-function eraseCookie(name) {
-  createCookie(name,"",-1);
+  if (readCookie('pushalert_10826_1_already_subscribed')) {
+document.getElementById('subscribed').click();
+$('.sidebar-box').removeClass('sidebar-box');
+$('.read-more').hide();
 }
